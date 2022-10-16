2b846bda90eadf72ec330ff8c9fe2d2f4ae5ba05
Colly7 <collins.ochola17@gmail.com>
2022-10-16 13:13:40 -0700

    Print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int letter = 97;
+
+	while (letter < 123)
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar(10);
+
+	return (0);
+}
