84eea1f9f122d3c6cd085138a0a7142282e4b470
Colly7 <collins.ochola17@gmail.com>
2022-10-16 13:24:58 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing the letters of the alphabets
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 122;
+
+	while (i > 96)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar(10);
+
+	return (0);
+}
